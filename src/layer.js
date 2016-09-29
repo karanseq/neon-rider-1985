8c30073d17bbdd1641321088e8d3c502ebc8cdbf
Karan Sequeira <u1087117@ad.utah.edu>
2016-09-29 09:14:29 -0600

    Outer layer turns red while blinking...

--- src/layer.js
+++ src/layer.js
@@ -66,6 +66,8 @@ Layer.prototype.startBlinking = function() {
 
 	this.blinkSound.play();
 
+	this.setColorRed();
+
 	var duration = 0;
 	this.blinkTweens.push(Game.add.tween(this.sprite).to({ alpha: 0.1 }, 1000, Phaser.Easing.Linear.None, true, duration, 0, true));
 	duration += 2000;
@@ -94,6 +96,8 @@ Layer.prototype.stopBlinking = function() {
 
 	this.blinkSound.stop();
 
+	this.setColorOriginal();
+
 	// stop and clear all tweens
 	while (this.blinkTweens.length > 0) {
 		var tween = this.blinkTweens.pop();
@@ -102,83 +106,6 @@ Layer.prototype.stopBlinking = function() {
 	
 	this.blinkTweens = null;
 };
-
-// Layer.prototype.startBlinking = function() {
-// 	// don't blink if we're already blinking
-// 	if (this.blinkTweens != null) {
-// 		return;
-// 	}
-// 	this.blinkPhase = 0;
-// 	this.blinkTweens = new Array();
-
-// 	this.doBlink();
-// 	this.finishBlinkEvent = Game.time.events.repeat(1000, 3, this.finishBlink, this);
-
-// 	// return total blink duration to layer manager
-// 	return 4000;
-// };
-
-// Layer.prototype.doBlink = function() {
-// 	var blinkDuration = 75;
-
-// 	this.setColorRed();
-// 	this.blinkTweens.push(Game.time.events.add(blinkDuration, this.setColorOriginal, this));
-// };
-
-// Layer.prototype.finishBlink = function() {
-// 	// stop any ongoing blinks
-// 	while (this.blinkTweens.length > 0) {
-// 		var tween = this.blinkTweens.pop();
-// 		Game.time.events.remove(tween);
-// 	}
-// 	this.blinkTweens = null;
-
-// 	++this.blinkPhase;
-// 	if (this.blinkPhase <= 3) {
-// 		this.blinkTweens = new Array();
-// 		this.doBlink();
-// 	}
-
-// 	switch (this.blinkPhase) {
-// 		case 1:
-// 		this.doBlinkEvent = Game.time.events.repeat(500, 1, this.doBlink, this);
-// 		break;
-
-// 		case 2:
-// 		this.doBlinkEvent = Game.time.events.repeat(250, 3, this.doBlink, this);
-// 		break;
-
-// 		case 3:
-// 		this.doBlinkEvent = Game.time.events.repeat(125, 7, this.doBlink, this);
-// 		break;
-// 	}
-// };
-
-// Layer.prototype.stopBlinking = function() {
-// 	// check if we're blinking
-// 	if (this.blinkTweens == null) {
-// 		return;
-// 	}
-
-// 	// stop and clear all tweens
-// 	while (this.blinkTweens.length > 0) {
-// 		var tween = this.blinkTweens.pop();
-// 		Game.time.events.remove(tween);
-// 	}
-// 	this.blinkTweens = null;
-
-// 	if (this.doBlinkEvent != null) {
-// 		Game.time.events.remove(this.doBlinkEvent);
-// 		this.doBlinkEvent = null;
-// 	}
-
-// 	if (this.finishBlinkEvent != null) {
-// 		Game.time.events.remove(this.finishBlinkEvent);
-// 		this.finishBlinkEvent = null;
-// 	}
-
-// 	this.setColorOriginal();
-// };
 
 Layer.prototype.setVisible = function(visibility) {
 	this.sprite.visible = visibility;
